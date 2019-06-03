76ec6e8d772cdfdcb4032f0927e87c9502d62fab
Andreas Gohr <andi@splitbrain.org>
2019-06-03 21:55:43 +0200

    some cleanup
    * use a single method, there wasn't really any need to split the
      functionality into two
    * use $conf['indexdir'] as it may be configured to be different from the
      default savedir
    * skip hidden pages
    * skip pages that no longer exist
    * use send_redirect() instead of sending a Location header only

--- action.php
+++ action.php
@@ -3,34 +3,33 @@
  *
  * @author     Szymon Olewniczak
  */
- 
+
 if(!defined('DOKU_INC')) die();
- 
- 
+
+
 class action_plugin_randompage2 extends DokuWiki_Action_Plugin {
 
- 
+
     public function register(Doku_Event_Handler $controller) {
         $controller->register_hook('ACTION_ACT_PREPROCESS', 'BEFORE', $this, 'do_randompage');
     }
 
     public function do_randompage(Doku_Event $event, $param) {
-        if($event->data !== 'randompage') return; 
+        if($event->data !== 'randompage') return;
         $event->preventDefault();
-        $this->action_randompage($event, $args);
-    }
-    
-    function action_randompage(Doku_Event $event, $args) {
+
         global $conf;
+        $dir = $conf['indexdir'];
 
-        $dir = $conf['savedir'];
-
-        $pages = file($dir.'/index/page.idx');
+        $pages = file($dir.'/page.idx');
         shuffle($pages);
-        
+
         foreach ($pages as $page) {
+            $page = trim($page);
+            if(!page_exists($page)) continue;
+            if(isHiddenPage($page)) continue;
             if (auth_quickaclcheck($page)) {
-                header('Location: '.wl($page,'',true));
+                send_redirect(wl($page, '', true, '&'));
             }
         }
     }
